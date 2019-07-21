b01ce280066c50db039cea5543cc872a2c0f2511
Sirenko Anzhela <43783411+sirenkoanzhela@users.noreply.github.com>
2019-07-21 18:25:37 +0300

    Save\Load in binary file

--- Storage_Devices.cpp
+++ Storage_Devices.cpp
@@ -20,6 +20,7 @@ void StorageDevice::Input()
 
 void StorageDevice::Print()
 {
+
 	std::cout << "Name: " << name << std::endl;
 	std::cout << "Model: " << model << std::endl;
 	std::cout << "Storage capacity: " << storage_capacity << std::endl;
@@ -28,10 +29,49 @@ void StorageDevice::Print()
 
 void StorageDevice::Save()
 {
+	FILE*f;
+	fopen_s(&f, "data.bin", "wb");
+	if (f == nullptr)
+	{
+		std::cout << "Cannot open this file!" << std::endl;
+		return;
+	}
+	int sizeName = name.size();
+	fwrite(&sizeName, sizeof(int), 1, f);
+	fwrite(name.c_str(), sizeof(char), sizeName, f);
 
+	int sizeModel = model.size();
+	fwrite(&sizeModel, sizeof(int), 1, f);
+	fwrite(model.c_str(), sizeof(char), sizeModel, f);
+
+	fclose(f);
 }
 
 void StorageDevice::Load()
 {
+	FILE*f;
+	fopen_s(&f, "data.bin", "rb");
+	if (f == nullptr)
+	{
+		std::cout << "Cannot open this file!" << std::endl;
+		return;
+	}
+	int size;
+	fread(&size, sizeof(int), 1, f);
+	std::string _name;
+	char*PtrName = new char[size];
+	fread(PtrName, size, 1, f);
+	_name = PtrName;
 
+
+	fread(&size, sizeof(int), 1, f);
+	std::string _model;
+	char*PtrModel = new char[size];
+	_model = PtrModel;
+
+
+	std::cout << _name << std::endl;
+	std::cout << _model << std::endl;
+	std::cout << "test closed" << std::endl;
+	fclose(f);
 }
